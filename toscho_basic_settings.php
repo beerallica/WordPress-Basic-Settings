b5aa25e4c3d2f300100e28011f97b854728ebdbe
beerallica <16150567+beerallica@users.noreply.github.com>
2017-11-26 12:46:36 +0200

    Update toscho_basic_settings.php

--- toscho_basic_settings.php
+++ toscho_basic_settings.php
@@ -1,31 +1,31 @@
 <?php
 /*
-Plugin Name: Toscho's basic settings
+Plugin Name: WP Custom Settings
 Plugin URI:  http://toscho.de/2010/wordpress-grundeinstellungen-per-plugin-setzen/
-Description: Some useful default configuration settings. See 'wp-admin/options.php' for more options. 
-Version:     0.3
+Description: Some useful default configuration settings. See https://codex.wordpress.org/Option_Reference for more options. 
+Version:     0.4
 Author:      Thomas Scholz
 Author URI:  http://toscho.de
 */
 
-function set_toscho_defaults()
+function set_wpc_settings_defaults()
 {
     global $wpdb;
 
     $o = array(
         'avatar_default'            => 'blank',
         'avatar_rating'             => 'G',
-        'category_base'             => '/thema',
+        'category_base'             => '/cat',
         'comment_max_links'         => 0,
         'comments_per_page'         => 0,
         'date_format'               => 'd.m.Y',
         'default_ping_status'       => 'closed',
         'default_post_edit_rows'    => 30,
         'links_updated_date_format' => 'j. F Y, H:i',
-        'permalink_structure'       => '/%year%/%postname%/',
-        'rss_language'              => 'de',
+        'permalink_structure'       => '/%category%/%postname%/',
+        'rss_language'              => 'en',
         'start_of_week'             => 1,
-        'timezone_string'           => 'Etc/GMT-1',
+        'timezone_string'           => 'Etc/GMT+2',
         'use_smilies'               => 0
     );
 
@@ -43,4 +43,4 @@ function set_toscho_defaults()
 
     return;
 }
-register_activation_hook(__FILE__, 'set_toscho_defaults');
+register_activation_hook(__FILE__, 'set_wpc_settings_defaults');
